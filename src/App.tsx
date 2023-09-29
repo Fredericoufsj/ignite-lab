4cb1282974546833582b82c5cf47911c8c65f0b1
Frederico Thomaz <frederico.santos@supersim.com.br>
2023-09-29 09:04:38 -0300

    commented code removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,10 @@ import { ApolloProvider, gql, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { client } from "./lib/apollo";
-import { Event } from "./pages/Event";
 import { Router } from "./Router";
 
-// const GET_LESSONS_QUERY = gql`
-//   query {
-//     lessons {
-//       id
-//       title
-//     }
-//   }
-// `
-// interface Lesson{
-//   id: string;
-//   title: string;
-// }
-
 function App() {
-  // useEffect(() => {
-  //   client.query({
-  //       query: GET_LESSONS_QUERY,
-  //     }).then((response) => {
-  //       console.log(response.data)
-  //     });
-  // }, []);
-
-  // const{data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
   return (
-    // <ul>
-    //   {data?.lessons.map(lesson => {
-    //     return <li key={lesson.id}>{lesson.title}</li>
-    //   })}
-    // </ul>
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Router />
